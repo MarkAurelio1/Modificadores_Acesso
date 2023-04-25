fce56981fa07ff00101b1aff446df2e86f07f21b
Marcos Aurelio <126610050+MarkAurelio1@users.noreply.github.com>
2023-04-25 19:13:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ModificadoresDeAcesso
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+            Console.WriteLine("Hello World");
+        }
+        // private classe fica privada
+        // , protected apenas itens filho ou seja que herda do pai consegue acessa
+        // , internal 
+        // e public base.  Para usar henrença
+        public class Pagamento
+        {   // Propriedades
+            DateTime Vencimento;
+            // Métodos
+            void Pagar() { }
+        }
+    }
+}
